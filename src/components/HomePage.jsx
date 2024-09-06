969332e69f02345767c33d43f32945efd0893515
NarutoPuipui <126446420+NarutoPuipui@users.noreply.github.com>
2024-09-07 01:08:56 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -17,7 +17,7 @@ export default function HomePage() {
       
       <Herosection/>
       <section className="mb-8">
-        <h2 className="text-4xl p-10 text-center font-bold mb-4 text-white">Top Trending Cryptocurrencies</h2>
+        <h2 className=" text-xl md:text-4xl p-10 text-center font-bold mb-4 text-white">Top Trending Cryptocurrencies</h2>
         <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mx-16">
         {data.coinData.map((coin) => (
             <Link to={`/${coin.id}`} key={coin.id} className="p-4 bg-black border border-red-500 rounded shadow-lg hover:animate-glow hover:border-4">
