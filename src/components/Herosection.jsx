87397bc023fd88570cf2afa058b3d5afd17e6832
NarutoPuipui <126446420+NarutoPuipui@users.noreply.github.com>
2024-09-07 00:41:56 +0530

    Update Herosection.jsx

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Herosection() {
   return (
     <div className='p-7 mt-10  flex justify-center items-center text-center rounded-3xl bg-hero-pattern4 w-[80vw] mx-auto h-[50vh] bg-cover'>
-        <h1 className=' text-white font-bold md:font-extrabold md:text-5xl text-xl text-center animate-lighting '>Stay Ahead of the Curve: Track Your Crypto Gains in Real-Time!</h1>
+        <h1 className=' text-white font-bold md:font-extrabold md:text-5xl text-sm text-center animate-lighting '>Stay Ahead of the Curve: Track Your Crypto Gains in Real-Time!</h1>
     </div>
   )
 }
