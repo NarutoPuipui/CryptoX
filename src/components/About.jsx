32724e11b250f40a02bbf3452cda312f15ff0a84
NarutoPuipui <126446420+NarutoPuipui@users.noreply.github.com>
2024-09-07 01:04:43 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
           CryptoX is a simple app to help you monitor the cryptocurrency market. It fetches data from the CoinGecko API to provide real-time prices and market data.
         </p>
         <p>
-          Developed by Mohit Singh. Check out my portfolio at {https://narutopuipui.github.io/MyPortfolio/}.
+          Developed by Mohit Singh. Check out my portfolio at (https://narutopuipui.github.io/MyPortfolio/).
         </p>
       </div>
     );
