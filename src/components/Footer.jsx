3f9a1e98f7f3d9dc2dbc5f36eaaa0a4137be5cef
NarutoPuipui <126446420+NarutoPuipui@users.noreply.github.com>
2024-09-07 00:32:11 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className=" box-border bg-black text-white sticky bottom-0 w-full ">
+      <footer className=" box-border bg-black text-white fixed bottom-0 w-full ">
         <div className="container mx-auto p-2 flex justify-between items-center">
           <p>&copy; 2024 Crypto Tracker. All rights reserved.</p>
           
