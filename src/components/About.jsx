fced20ed17030fca100b8f53211fc5afacb72584
NarutoPuipui <126446420+NarutoPuipui@users.noreply.github.com>
2024-09-07 00:35:43 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,10 @@ export default function AboutPage() {
       <div className="container mx-auto p-4 text-white">
         <h2 className="text-xl font-bold mb-4">About Crypto Tracker</h2>
         <p>
-          Crypto Tracker is a simple app to help you monitor the cryptocurrency market. It fetches data from the CoinGecko API to provide real-time prices and market data.
+          CryptoX is a simple app to help you monitor the cryptocurrency market. It fetches data from the CoinGecko API to provide real-time prices and market data.
         </p>
         <p>
-          Developed by Your Name. Check out my portfolio [here](#).
+          Developed by Mohit Singh. Check out my portfolio at {https://narutopuipui.github.io/MyPortfolio/}.
         </p>
       </div>
     );
